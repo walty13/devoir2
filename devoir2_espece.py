ba1752eb5f2a7870a0291b3be3d07be9c4c7a95b
walty13 <walterbeles@gmail.com>
2023-02-04 00:12:36 -0500

    last push from windows

--- devoir2_espece.py
+++ devoir2_espece.py
@@ -25,25 +25,23 @@ href : http://www-laplace.inrialpes.fr/publications/Rayons/Tapus02.pdf
 
 # nombre d'états
 nb_states = 3
-states = {"population en augmentation" : 0, "population stable" : 1, "population en declin" : 2}
+states = {0:"population en augmentation", 1:"population stable", 2:"population en declin"}
 # nombre d'actions de transition
 nb_actions = 2
-actions = {"zones protegees" : 0, "reintroduction de specimens" : 1}
-# nombre de semestre d'observation
-N = 4
+actions = {0:"zones protegees", 1:"reintroduction de specimens"}
+# nombre de trimestres d'observation
+N = 5
 
 # matrice de transition
-a = [[[0.6, 0.3, 0.1],
+P = [[[0.7, 0.3, 0.0],
       [0.2, 0.6, 0.2],
-      [0.1, 0.4, 0.5]],
-    [[0.6, 0.2, 0.2],
+      [0.0, 0.4, 0.6]],
+    [[0.6, 0.3, 0.1],
       [0.3, 0.5, 0.2],
-      [0.2, 0.3, 0.5]]]
+      [0.1, 0.4, 0.5]]]
 
 # récompenses
-R = [[2,0,-1],
-     [-1,1,-1],
-     [1,0,-2]]
+R = [4,2,-3]
 
 # J(x) : population à l'étape k (semestre k) si le client a pris ses decisions de facon optimale
 J = np.zeros((N+1,nb_states))
@@ -54,28 +52,25 @@ mu = np.zeros((N+1,nb_states))
 policy = np.zeros((nb_actions))
 profits = np.zeros((nb_actions))
 
-
 # population initiale
 population = 60
 # initialisation de J
-for i in range(nb_states):
-    for j in range(nb_actions):
-        profits[j] = np.dot(a[j][i],R[i]) + population
-    J[N][i] = np.max(profits)
-    mu[N][i] = np.argmax(profits)
 
-print("J(5): {0:.2f} | {1:.2f} | {2:.2f}".format(J[N][0],J[N][1],J[N][2]))
-print("mu(5): {0}, {1}, {2}".format(mu[N][0],mu[N][1],mu[N][2]), end="\n\n")
+for i in range(nb_states):
+    for a in range(nb_actions):
+        profits[a] = R[i] + np.sum([P[a][i][j] for j in [k  for k in range(nb_states) if k!=i]])
+    J[N-1][i] = np.max(profits)
+    mu[N-1][i] = np.argmax(profits)
+
+print("J(5): {0:.2f} | {1:.2f} | {2:.2f}".format(J[N-1][0],J[N-1][1],J[N-1][2]))
+print("mu(5): {0}, {1}, {2}".format(mu[N-1][0],mu[N-1][1],mu[N-1][2]), end="\n\n")
 
 # boucle principale
 for t in reversed(range(N)):
     for i in range(nb_states):
-        max_value = -1e9
-        best_a = -1
-        value = 0
-        for j in range(nb_actions):
-            profits[j] = np.dot(a[j][i],R[i]) + J[t+1][i]
-        J[t][i] = np.max(profits)
+        for a in range(nb_actions):
+            profits[a] = R[i] + np.sum([P[a][i][j]*J[t+1][i] for j in [k  for k in range(nb_states) if k!=i]])
+        J[t][i] = np.max(profits[a])
         print("J({0})[{1}]: {2:.2f} ".format(t+1, i, J[t][i]), end="| ")
         mu[t][i] = np.argmax(profits)
         print("mu({0})[{1}]: {2}".format(t+1, i, mu[t][i]))
@@ -90,9 +85,11 @@ for t in reversed(range(N)):
 
 #Affichage de la valeur optimale et de la politique optimale
 print("Resultats...")
-J_0 = np.max(J[0])
+J_0 = population + np.max(J[0])
 print("J({0}): {1:.2f} ".format(0, J_0))
 print("Finalement la strategie optimale est :")
-for i in range(N):
-    print("{"+str(mu[i])+"}", end=",")
+print([[int(mu[t][i]) for i in range(nb_states)] for t in range(N-1)])
 
+print("De manière générale il faut adopter la politique suivante pour chacun des etats :")
+opti = np.matrix([[int(mu[t][i]) for i in range(nb_states)] for t in range(N-1)]).mean(0)
+print([actions[np.array(opti)[0][i]] for i in range(nb_states)])
--- devoir2_roulette.py
+++ devoir2_roulette.py
@@ -1,104 +1,75 @@
 import numpy as np
 
 """
-L'exemple que j'ai choisit est le suivant : 
-Un client souhaite utiliser un bot pour gagner un tournoi de roulette en ligne.
-Pour cela il me demande de définir la stratégie qui maximiser ses gains en partant d'un 
-budget fixé. Mon client n'a manifestement pas beaucoup de connaissances sur le jeu de 
-la roulette et souhaite investir le moins d'argent dans cette implémentation pour 
-maximiser son profit total.
+Un exemple où la programmation dynamique markovienne peut être utilisée est dans la modélisation de l'évolution d'une espèce 
+dans un environnement modifié par l'homme.
 
-Pour cela je recommande d'utiliser une méthode de programmation dynamique. 
-Je lui explique que pour le jeu de la roulette est basé sur des chances aléatoires 
-de gagner avec des probabilités qui varient en fonction de la manière de jouer. 
-En effet on peut placer 2 types de paris différents à chaque partie : les paris extérieurs 
-et les paris intérieurs. Un pari intérieur s'applique à un seul ou un groupe de numéros.
-Ces paris sont les plus difficiles à gagner, mais ils sont également les plus gratifiants 
-lorsque vous le faites. Inversement, un pari extérieur est un pari sur un grand groupe tel 
-que impair/pair, rouge/noir, ou 1 à 18 et 19 à 36. Vous avez beaucoup plus de chances de gagner 
-quand vous optez pour les paris externes, mais ne vous attendez pas à un gain énorme.
+Client : 
 
-On peut alors modéliser ce problème comme le processus de décision markovien :
+On modélise le problème comme ceci :
+    états : population stable, population en augmentation, population en déclin.
+    actions : mise en place de zones protégées, la réintroduction de spécimens sauvages
+    incertitudes: problème stochastique car facteurs incertains nombreux (alimentation, environnement, reproduction, etc...)
 
-Les 3 états sont :
-- Perdant
-- Gagnant à moitié
-- Gagnant totalement
+Nous pouvons utiliser la programmation dynamique markovienne pour déterminer la politique optimale pour chaque état de la population de l'espèce.
+Nous considérons que l'état de la population de l'espèce change au fil du temps en fonction des actions prises par l'équipe de conservation.
 
-Les 2 actions sont :
-- Continuer à jouer
-- Quitter le jeu
+En utilisant les équations de récurrence de Bellman, nous pouvons déterminer la valeur de chaque état et les politiques optimales pour chaque étape.
+Nous pourrions montrer à l'équipe de conservation un tableau comparant les coûts et les effets sur la population de l'espèce pour chaque politique envisagée.
+Cela permettrait de leur montrer la valeur de la programmation dynamique markovienne pour prendre des décisions sous incertitude dans ce genre de situation.
 
-La transition entre les états dépendra de la chance et des décisions prises par le joueur. 
-Par exemple, si le joueur est dans un état de "Gagnant à moitié", la probabilité de passer 
-à un état de "Gagnant totalement" sera plus élevée si le joueur continue à jouer plutôt que 
-de quitter le jeu. Si le joueur est dans un état de "Perdant", la probabilité de passer 
-à un état de "Gagnant à moitié" sera plus élevée si le joueur continue à jouer plutôt que 
-de quitter le jeu.
-
-Le processus de décision markovien dans ce jeu sera donc basé sur la maximisation du 
-gain potentiel du joueur à chaque tour, en prenant en compte les probabilités associées 
-à chaque action et état
+href : http://www-laplace.inrialpes.fr/publications/Rayons/Tapus02.pdf
 
 """
 
 # nombre d'états
 nb_states = 3
-X = {"gagnant total" : 0, "gagnant partiel" : 1, "perdant" : 2}
+states = {0:"population en augmentation", 1:"population stable", 2:"population en declin"}
 # nombre d'actions de transition
 nb_actions = 2
-U = {"continuer" : 0, "quitter" : 1}
-# nombre de semaines d'observation
+actions = {0:"zones protegees", 1:"reintroduction de specimens"}
+# nombre de trimestres d'observation
 N = 5
 
-# matrice de transition : 
-# on fixe les probabilités de gagner en fonction de la stratégie adopté à la partie k
-D = [[0.05, 0.07, 0.9],
-     [0.04, 0.05, 0.05],
-     [, , ]]
+# matrice de transition
+P = [[[0.7, 0.3, 0.0],
+      [0.2, 0.6, 0.2],
+      [0.0, 0.4, 0.6]],
+    [[0.6, 0.3, 0.1],
+      [0.3, 0.5, 0.2],
+      [0.1, 0.4, 0.5]]]
 
 # récompenses
-R = [[.25,.15],
-     [.15,.12],
-     [-.05,.08]]
+R = [4,2,-3]
 
-# J(x) : budget à l'étape k (semestre k) si le client a pris ses decisions de facon optimale
+# J(x) : population à l'étape k (semestre k) si le client a pris ses decisions de facon optimale
 J = np.zeros((N+1,nb_states))
 # mu(x) : politique optimale a l'etat k
 mu = np.zeros((N+1,nb_states))
 
-# budget initiale
-budget = 100000
 # profits
 policy = np.zeros((nb_actions))
 profits = np.zeros((nb_actions))
 
-# fonction de transition
-def transition(x, u, D):
-    return D[u]*(1+np.log())
-
-def cout(x, u, D):
-    return R[x][u] + budget
-
+# population initiale
+population = 60
 # initialisation de J
-for i in range(nb_states):
-    for j in range(nb_actions):
-        profits[j] = transition(t[i],R[i][j])
-    J[N][i] = np.max(profits)
-    mu[N][i] = np.argmax(profits)
 
-print("J(5): {0:.2f} | {1:.2f} | {2:.2f}".format(J[N][0],J[N][1],J[N][2]))
-print("mu(5): {0}, {1}, {2}".format(mu[N][0],mu[N][1],mu[N][2]), end="\n\n")
+for i in range(nb_states):
+    for a in range(nb_actions):
+        profits[a] = R[i] + np.sum([P[a][i][j] for j in [k  for k in range(nb_states) if k!=i]])
+    J[N-1][i] = np.max(profits)
+    mu[N-1][i] = np.argmax(profits)
+
+print("J(5): {0:.2f} | {1:.2f} | {2:.2f}".format(J[N-1][0],J[N-1][1],J[N-1][2]))
+print("mu(5): {0}, {1}, {2}".format(mu[N-1][0],mu[N-1][1],mu[N-1][2]), end="\n\n")
 
 # boucle principale
 for t in reversed(range(N)):
     for i in range(nb_states):
-        max_value = -1e9
-        best_a = -1
-        value = 0
-        for j in range(nb_actions):
-            profits[j] = np.dot(a[j][i],R[i]) + J[t+1][i]
-        J[t][i] = np.max(profits)
+        for a in range(nb_actions):
+            profits[a] = R[i] + np.sum([P[a][i][j]*J[t+1][i] for j in [k  for k in range(nb_states) if k!=i]])
+        J[t][i] = np.max(profits[a])
         print("J({0})[{1}]: {2:.2f} ".format(t+1, i, J[t][i]), end="| ")
         mu[t][i] = np.argmax(profits)
         print("mu({0})[{1}]: {2}".format(t+1, i, mu[t][i]))
@@ -113,9 +84,11 @@ for t in reversed(range(N)):
 
 #Affichage de la valeur optimale et de la politique optimale
 print("Resultats...")
-J_0 = np.max(J[0])
+J_0 = population + np.max(J[0])
 print("J({0}): {1:.2f} ".format(0, J_0))
 print("Finalement la strategie optimale est :")
-for i in range(N):
-    print("{"+str(mu[i])+"}", end=",")
+print([[int(mu[t][i]) for i in range(nb_states)] for t in range(N-1)])
 
+print("De manière générale il faut adopter la politique suivante pour chacun des etats :")
+opti = np.matrix([[int(mu[t][i]) for i in range(nb_states)] for t in range(N-1)]).mean(0)
+print([actions[np.array(opti)[0][i]] for i in range(nb_states)])
